72f6171a190d1a93bd53d9ac8aafec6d26c4a7f8
Androide18 <54455837+Androide18@users.noreply.github.com>
2020-09-01 21:05:37 -0300

    img.jpg

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,6 +8,9 @@ const router = Router();
 const multer = require('multer');
 const path = require('path');
 
+
+
+
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 // router.use('/auth', authRouter);
@@ -15,6 +18,8 @@ router.use('/products', productRouter);
 router.use('/products/new', productRouter);
 router.use('/products/:id', productRouter);
 router.use('/uploads', productRouter);
+
+
 
 
 module.exports = router;
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -3,17 +3,40 @@ const { Product } = require('../db.js');
 
 const multer = require('multer');
 const path = require('path');
- var upload = multer({ dest: 'uploads/' })
+var upload = multer({ dest: 'uploads/' })
 
+
+const storage = multer.diskStorage({
+	destination: path.join(__dirname, '/uploads'),
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	}
+})
+
+
+	// POST IMAGE CON MULTER
+	//  server.post('/', upload.single('image'), (req, res) => {
+		
+	// 	res.send('HOLA')
+	// 	console.log(req.file, req.body);
+	// });
+	const uploadImage = multer({
+		storage,
+		limits: {fileSize: 1000000}
+	}).single('image');
 
 	
-	// POST IMAGE CON MULTER
-	 server.post('/', upload.single('image'), (req, res) => {
-		res.send('HOLA')
-		console.log(req.file, req.body);
+
+	server.post('/', (req, res) => {
+		uploadImage(req, res, (err) => {
+			if (err) {
+				err.message = 'Ta re pesada la imagen amigx!';
+				return res.send(err);
+			}
+			console.log(req.file);
+			res.send('La imagen se subio bien !');
+		});
 	});
-
-
 
 
 
