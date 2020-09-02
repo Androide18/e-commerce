49a036df873b5d55c62bdf9659e4e346ed52ec70
Androide18 <54455837+Androide18@users.noreply.github.com>
2020-09-02 18:47:58 -0300

    modelo usuario hecho

---
+++ api/src/models/User.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+    sequelize.define('user', {
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+              isEmail:true
+            },
+            unique: {
+                args: true,
+                msg: 'Email address already in use!'
+            }
+          },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE,
+    });
+};
