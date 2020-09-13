7c14cc51a3ed0b1769ddfba834b2a80ff828dacd
Fabricio R. Mores <fabricio@MacBook-Pro-de-Fabricio.local>
2020-09-13 19:29:20 -0300

    conexion redux 90%

--- client/src/components/CreateProductScreen.js
+++ client/src/components/CreateProductScreen.js
@@ -1,27 +1,22 @@
 import '../App.css';
-import axios from "axios";
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { Modal, ModalBody, ModalFooter, ModalHeader, FormGroup, Label, Input } from 'reactstrap';
-import { deleteProduct, getProducts } from '../actions/ProductsActions';
+import { deleteProduct, getProducts, saveProduct, updateProduct } from '../actions/ProductsActions';
 
 
-
-const url = "http://localhost:3001/products/";
-const urlCat = "http://localhost:3001/categories/";
 
 function CreateProductsScreen() {
 
   const dispatch = useDispatch();
   const [form, setForm] = useState({ id: '', name: '', price: '', stock: '', description: '', category: '', brand: '', image: '', tipoModal: '' })
-  const data = useSelector(state => state.product.productsLoaded)
+  const { productsLoaded, loading, error } = useSelector(state => state.product)
   const dataCat = useSelector(state => state.categories.categoriesLoaded)
   const [modalInsertar, setModalInsertar] = useState(false);
   const [modalEliminar, setModalEliminar] = useState(false);
-
 
   const peticionPost = async () => {
     delete form.id;
@@ -29,44 +24,22 @@ function CreateProductsScreen() {
       formData.append(key, value);
       return formData;
     }, new FormData());
-
-    await axios.post(url, formData,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      }
-    ).then(res => {
-      modalInsert();
-    }).catch(error => {
-      console.log(error.message);
-    })
+    modalInsert();
+    await dispatch(saveProduct(formData));
+    await dispatch(getProducts());
   }
 
+  const peticionPut = async () => {
+    modalInsert();
+    await dispatch(updateProduct(form.id, form));
+    await dispatch(getProducts());
 
-  const peticionPut = () => {
-    axios.put(url + form.id, form)
-      .then(res => {
-        modalInsert();
-        
-        console.log(res);
-      })
   }
 
-  // const peticionDelete = () => {
-  //   axios.delete(url + form.id)
-  //     .then(res => {
-  //       setModalEliminar(false);
-        
-  //       console.log(res);
-  //     })
-  // }
-
-  const peticionDelete = () => {
-      dispatch(deleteProduct(form.id));
-      setModalEliminar(false);
-      dispatch(getProducts);
-      console.log(form.id);
+  const peticionDelete = async () => {
+    await dispatch(deleteProduct(form.id));
+    await dispatch(getProducts());
+    setModalEliminar(false);
   }
 
   const modalInsert = (adfasdf) => {
@@ -110,147 +83,152 @@ function CreateProductsScreen() {
   }
 
   return (
-    <div className="App">
-      <br /><br /><br />
-      <button className="btn btn-success"
-        action='/uploads'
-        method='POST'
-        encType='multipart/form-data'
-        onClick={() => {
-          setForm({ tipoModal: 'insertar' });
-          modalInsert()
-        }}>Agregar Producto</button>
-      <br /><br />
-      <table className="table ">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Marca</th>
-            <th>Precio</th>
-            <th>Stock</th>
-            <th>Descripcion</th>
-            <th>Categoria</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(prod => {
-            return (
-              <tr key={prod.id}>
-                <td>{prod.id}</td>
-                <td>{prod.name}</td>
-                <td>{prod.brand}</td>
-                <td>${prod.price}</td>
-                <td>{prod.stock}u</td>
-                <td>{prod.description}</td>
-                <td>{prod.category}</td>
-                <td><img src={`http://localhost:3001/static/${prod.image}`} class="image-thumbnail" /></td>
-                <td>
-                  <button className="btn btn-primary" //boton editar
-                    onClick={() => {
-                      selectedProduct(prod);
-                      modalInsert()
-                    }}><FontAwesomeIcon icon={faEdit} /></button>
-                  {"   "}
-                  <button className="btn btn-danger"  // boton eliminar
-                    onClick={() => {
-                      selectedProduct(prod);
-                      setModalEliminar(true)
-                    }}><FontAwesomeIcon icon={faTrashAlt} /></button>
-                </td>
+    <>
+      <br />
+      {loading ? (<div>Loading...</div>) : error ? (<div>no esta funcionando{error}</div>) : (
+        <div className="App">
+          <br /><br /><br />
+          <button className="btn btn-success"
+            action='/uploads'
+            method='POST'
+            encType='multipart/form-data'
+            onClick={() => {
+              setForm({ tipoModal: 'insertar' });
+              modalInsert()
+            }}>Agregar Producto</button>
+          <br /><br />
+          <table className="table ">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Marca</th>
+                <th>Precio</th>
+                <th>Stock</th>
+                <th>Descripcion</th>
+                <th>Categoria</th>
+                <th>Image</th>
               </tr>
-            )
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {productsLoaded.map(prod => {
+                return (
+                  <tr key={prod.id}>
+                    <td>{prod.id}</td>
+                    <td>{prod.name}</td>
+                    <td>{prod.brand}</td>
+                    <td>${prod.price}</td>
+                    <td>{prod.stock}u</td>
+                    <td>{prod.description}</td>
+                    <td>{prod.category}</td>
+                    <td><img src={`http://localhost:3001/static/${prod.image}`} class="image-thumbnail" /></td>
+                    <td>
+                      <button className="btn btn-primary" //boton editar
+                        onClick={() => {
+                          selectedProduct(prod);
+                          modalInsert()
+                        }}><FontAwesomeIcon icon={faEdit} /></button>
+                      {"   "}
+                      <button className="btn btn-danger"  // boton eliminar
+                        onClick={() => {
+                          selectedProduct(prod);
+                          setModalEliminar(true)
+                        }}><FontAwesomeIcon icon={faTrashAlt} /></button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
 
-      <Modal isOpen={modalInsertar}>
-        <ModalHeader style={{ display: 'block' }}>
-          <span style={{ float: 'right' }} onClick={() => modalInsert()}>x</span>
-        </ModalHeader>
-        <ModalBody>
-          <form action='/uploads'
-            method='POST' encType='multipart/form-data' >
-            <div className="form-group">
-              <label htmlFor="id">ID</label>
-              <input className="form-control"
-                type="text" name="id" id="id"
-                readOnly onChange={() => handleChange}
-                value={form ? form.id : data.length + 1} />
-              <br />
-              <label htmlFor="name">Nombre</label>
-              <input className="form-control"
-                type="text" name="name" id="name"
-                onChange={handleChange} value={form ? form.name : ''} />
-              <br />
-              <label htmlFor="brand">Marca</label>
-              <input className="form-control"
-                type="text" name="brand" id="brand"
-                onChange={handleChange} value={form ? form.brand : ''} />
-              <br />
-              <label htmlFor="price">Precio</label>
-              <input className="form-control"
-                type="number" name="price" id="price"
-                onChange={handleChange} value={form ? form.price : ''} />
-              <br />
-              <label htmlFor="stock">Stock</label>
-              <input className="form-control"
-                type="number" name="stock" id="stock"
-                onChange={handleChange} value={form ? form.stock : ''} />
-              <br />
-              <FormGroup>
-                <Label for="exampleSelect">Categorias</Label>
-                <Input type="select" name="category" id="category"
-                  onChange={handleChange}
-                  value={form ? form.category : ''}>
-                  {
-                    dataCat.map(e => {
-                      return <option className='' key={e.id}>{e.name}</option>
-                    })
-                  }
-                </Input>
-              </FormGroup>
-              <br />
-              <label htmlFor="description">Descripcion</label>
-              <input className="form-control"
-                type="text" name="description" id="description"
-                onChange={handleChange} value={form ? form.description : ''} />
-              <br />
-              <label htmlFor="image">Imagen</label>
-              <input type="file" name="image" id="image" alt="imagen"
-                onChange={handleFileChange} value=''
-              //onChange={(e) => {this.uploadImage(e)}}
-              />
-              <br />
-            </div>
-          </form>
-        </ModalBody>
+          <Modal isOpen={modalInsertar}>
+            <ModalHeader style={{ display: 'block' }}>
+              <span style={{ float: 'right' }} onClick={() => modalInsert()}>x</span>
+            </ModalHeader>
+            <ModalBody>
+              <form action='/uploads'
+                method='POST' encType='multipart/form-data' >
+                <div className="form-group">
+                  <label htmlFor="id">ID</label>
+                  <input className="form-control"
+                    type="text" name="id" id="id"
+                    readOnly onChange={() => handleChange}
+                    value={form ? form.id : productsLoaded.length + 1} />
+                  <br />
+                  <label htmlFor="name">Nombre</label>
+                  <input className="form-control"
+                    type="text" name="name" id="name"
+                    onChange={handleChange} value={form ? form.name : ''} />
+                  <br />
+                  <label htmlFor="brand">Marca</label>
+                  <input className="form-control"
+                    type="text" name="brand" id="brand"
+                    onChange={handleChange} value={form ? form.brand : ''} />
+                  <br />
+                  <label htmlFor="price">Precio</label>
+                  <input className="form-control"
+                    type="number" name="price" id="price"
+                    onChange={handleChange} value={form ? form.price : ''} />
+                  <br />
+                  <label htmlFor="stock">Stock</label>
+                  <input className="form-control"
+                    type="number" name="stock" id="stock"
+                    onChange={handleChange} value={form ? form.stock : ''} />
+                  <br />
+                  <FormGroup>
+                    <Label for="exampleSelect">Categorias</Label>
+                    <Input type="select" name="category" id="category"
+                      onChange={handleChange}
+                      value={form ? form.category : ''}>
+                      {
+                        dataCat.map(e => {
+                          return <option className='' key={e.id}>{e.name}</option>
+                        })
+                      }
+                    </Input>
+                  </FormGroup>
+                  <br />
+                  <label htmlFor="description">Descripcion</label>
+                  <input className="form-control"
+                    type="text" name="description" id="description"
+                    onChange={handleChange} value={form ? form.description : ''} />
+                  <br />
+                  <label htmlFor="image">Imagen</label>
+                  <input type="file" name="image" id="image" alt="imagen"
+                    onChange={handleFileChange} value=''
+                  //onChange={(e) => {this.uploadImage(e)}}
+                  />
+                  <br />
+                </div>
+              </form>
+            </ModalBody>
 
-        <ModalFooter>
-          {form.tipoModal === 'insertar' ?
-            <button className="btn btn-success"
-              onClick={() => peticionPost()}>Insertar
-                  </button> : <button className="btn btn-primary"
-              onClick={() => peticionPut}>Actualizar</button>
-          }
-          <button className="btn btn-danger"
-            onClick={() => modalInsert()}>Cancelar</button>
-        </ModalFooter>
-      </Modal>
+            <ModalFooter>
+              {form.tipoModal === 'insertar' ?
+                <button className="btn btn-success"
+                  onClick={() => peticionPost()}>Insertar
+                    </button> : <button className="btn btn-primary"
+                  onClick={() => peticionPut()}>Actualizar</button>
+              }
+              <button className="btn btn-danger"
+                onClick={() => modalInsert()}>Cancelar</button>
+            </ModalFooter>
+          </Modal>
 
-      <Modal isOpen={modalEliminar}>
-        <ModalBody>
-          Estás seguro que deseas eliminar el producto {form && form.name}
-        </ModalBody>
-        <ModalFooter>
-          <button className="btn btn-danger"
-            onClick={() => peticionDelete()}>Sí</button>
-          <button className="btn btn-secundary"
-            onClick={() => setModalEliminar(false)}>No</button>
-        </ModalFooter>
-      </Modal>
-    </div>
+          <Modal isOpen={modalEliminar}>
+            <ModalBody>
+              Estás seguro que deseas eliminar el producto {form && form.name}
+            </ModalBody>
+            <ModalFooter>
+              <button className="btn btn-danger"
+                onClick={() => peticionDelete()}>Sí</button>
+              <button className="btn btn-secundary"
+                onClick={() => setModalEliminar(false)}>No</button>
+            </ModalFooter>
+          </Modal>
+        </div>
+      )}
+    </>
   )
 }
 export default CreateProductsScreen;
--- client/src/reducers/C-reducer.js
+++ client/src/reducers/C-reducer.js
@@ -1,10 +1,11 @@
-import {
-    DELETE_CATEGORY_ERROR, 
-    DELETE_CATEGORY_REQUEST, 
-    DELETE_CATEGORY_SUCCESS, 
-    GET_CATEGORIES_ERROR, 
-    GET_CATEGORIES_REQUEST, 
-    GET_CATEGORIES_SUCCESS} from '../constants-F/constanst'
+import { 
+    DELETE_CATEGORY_ERROR, DELETE_CATEGORY_REQUEST, DELETE_CATEGORY_SUCCESS,
+    GET_CATEGORIES_ERROR, GET_CATEGORIES_REQUEST, GET_CATEGORIES_SUCCESS, 
+    SAVE_CATEGORY_ERROR, SAVE_CATEGORY_REQUEST, SAVE_CATEGORY_SUCCESS, 
+    UPDATE_CATEGORY_ERROR, UPDATE_CATEGORY_REQUEST, UPDATE_CATEGORY_SUCCESS,
+} from "../constants-F/constanst";
+
+
 const initialState = {
     categoriesLoaded: [],
     loading: false,
@@ -24,7 +25,7 @@ function getCategoriesReducer(state = initialState, action) {
     }
 }
 
-function deleteCategoriesReducer(state = initialState, action) {
+function deleteCategoryReducer(state = initialState, action) {
     switch (action.type) {
         case DELETE_CATEGORY_REQUEST:
             return { loading: true };
@@ -37,7 +38,35 @@ function deleteCategoriesReducer(state = initialState, action) {
     }
 }
 
+function saveCategoryReducer(state = initialState, action) {
+    switch (action.type) {
+        case SAVE_CATEGORY_REQUEST:
+            return { loading: true };
+        case SAVE_CATEGORY_SUCCESS:
+            return { loading: false };
+        case SAVE_CATEGORY_ERROR:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+
+function updateCategoryReducer(state = initialState, action) {
+    switch (action.type) {
+        case UPDATE_CATEGORY_REQUEST:
+            return { loading: true };
+        case UPDATE_CATEGORY_SUCCESS:
+            return { loading: false };
+        case UPDATE_CATEGORY_ERROR:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
+
 export {
     getCategoriesReducer,
-    deleteCategoriesReducer
+    deleteCategoryReducer,
+    saveCategoryReducer,
+    updateCategoryReducer
 }
