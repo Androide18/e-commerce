e9eaa504796f5dd4dec6f98be82ddb12ea13be91
Androide18 <54455837+Androide18@users.noreply.github.com>
2020-09-18 12:11:09 -0300

    testing

--- api/src/routes/search.js
+++ api/src/routes/search.js
@@ -1,19 +1,38 @@
 const server = require('express').Router();
-const { Product, Category } = require('../db.js');
-
+const { Product } = require('../db.js');
+const { Op } = require("sequelize");
 
 
 server.get('/', (req, res) => {
-     const query  = req.query.name;
-     console.log(req.query);
-     Product.findAll({ where: { name: query } })
-         .then(result => {
-             res.send(result)
-         })
-         .catch(err => {
-             console.log(err, message);
-     })
-})
+    const  query  = req.query;
+    console.log(query)
+    if (!query) return res.send("error");
+  
+    Product.findAll({
+      where: {
+        [Op.or]: {
+          name: { [Op.like]: '%query%' },
+          description: { [Op.like]: '%query%' },
+        },
+      },
+    })
+      .then((products) => {
+        res.send(products);
+      })
+      .catch(() => res.send("hay un puto error"));
+  });
+
+
+    //      const query  = req.query.name;
+//      console.log(req.query);
+//      Product.findAll({ where: { name: query } })
+//          .then(result => {
+//              res.send(result)
+//          })
+//          .catch(err => {
+//              console.log(err, message);
+//      })
+// })
 
 
 module.exports = server;
